da62635a2d377274c8aa45634b16954b1c2b1789
leemadov <143035177+leemadov@users.noreply.github.com>
2023-11-04 10:26:43 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function adjustLayout() {
-    if (window.innerWidth < 600) {
-    } else {
-    }
-}
-
-window.addEventListener('load', adjustLayout);
-window.addEventListener('resize', adjustLayout);
